4795bc777916db491a9e290d4e1ea8d472cd5e00
Ivan Freire <ivanfreire87@gmail.com>
2015-08-02 14:20:48 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,7 +8,6 @@
 #include "utils.hh"
 #include "typechecker.hh"
 
-
 extern int yyparse();
 extern FILE *yyin;
 
@@ -19,33 +18,32 @@ static ASTTypesVecNode* typesVec_node = NULL;
 static SymbolTable symtab;
 
 
-int main(int argc, char** argv)
-{
-    cout << "Running minijavap compiler. \n"; cout.flush();
-    ASTUtils::init_ast_utils(symtab);
+int main(int argc, char** argv){
+	cout << "Running minijavap compiler. \n"; cout.flush();
+	ASTUtils::init_ast_utils(symtab);
    
-    ++argv, --argc;
-    yyin = (argc > 0) ? fopen(argv[0], "r") : stdin;
-    int yyres = yyparse();
-    if(yyres || not ASTUtils::get_varsok())
+    	++argv, --argc;
+    	yyin = (argc > 0) ? fopen(argv[0], "r") : stdin;
+    	int yyres = yyparse();
+    	if(yyres || not ASTUtils::get_varsok())
 		err_exit(cout, "Terminating due to parsing errors.");    
-    else{
+    	else{
 		cout << "Lexical analysis: passed." << endl;
 		cout << "Syntax analysis: passed." << endl;
-    }
-	typesVec_node = ASTUtils::get_typesVec_node();
-    root = ASTUtils::get_root_node();
-    if(root == NULL)
+    	}
+    	typesVec_node = ASTUtils::get_typesVec_node();
+    	root = ASTUtils::get_root_node();
+    	if(root == NULL)
 		err_exit(cout, "AST not build.");
     
-    TypeChecker typecheck;
-    typesVec_node->accept(typecheck);
-    root->accept(typecheck);
-    if(not typecheck.typeok){
+    	TypeChecker typecheck;
+    	typesVec_node->accept(typecheck);
+    	root->accept(typecheck);
+	if(not typecheck.typeok){
 		err_exit(cout, "Type checking: failed. Terminating ...");
 		exit(1);
-    }else
+    	}else
 		cout << "Type checking: passed." << endl;
     
-    return 0;
+    	return 0;
 }
